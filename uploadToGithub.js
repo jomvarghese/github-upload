c5d6b8711c2a78f41d97eab855135930109f6852
jomvarghese <jomscloud>
2024-12-15 15:29:17 +0530

    Token updated

--- uploadToGithub.js
+++ uploadToGithub.js
@@ -1,7 +1,7 @@
 const accessToken = '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';
 const repoOwner = 'jomvarghese';
 const repoName = 'github-upload';
-const filePath = 'WorkSpace'; // Path in the repo
+const filePath = 'WorkSpace/filename.ext'; // Path in the repo
 
 // Function to upload file to GitHub repository
 async function uploadFileToGithub(file) {
